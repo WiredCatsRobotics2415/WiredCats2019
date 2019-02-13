341f068cb5b6337cf20678025e4cdee41a56000c
MattP34 <mattthewpropp@westmister.net>
2019-02-13 12:48:55 -0500

    added some of the stuff for turn based pid

--- src/main/java/frc/robot/subsystems/VelocityDrive.java
+++ src/main/java/frc/robot/subsystems/VelocityDrive.java
@@ -40,31 +40,10 @@ public class VelocityDrive extends Subsystem {
    */
   private ShuffleboardTab manualPIDFTuner;
   /**
-   * shuffleboard value for P for PID tuning
+   * shuffleboard values
    */
-  private NetworkTableEntry networkTableKP;
-  /**
-   * shuffleboard value for I for PID tuning
-   */
-  private NetworkTableEntry networkTableKI;
-  /**
-   * shuffleboard value for D for PID tuning
-   */
-  private NetworkTableEntry networkTableKD;
-  /**
-   * shuffleboard value for F for PID tuning
-   */
-  private NetworkTableEntry networkTableKF;
-  /**
-   * shuffleboard value the error of the left motors
-   */
-  private NetworkTableEntry leftPIDError;
-  /**
-   * shuffleboard value the error of the right motors
-   */
-  private NetworkTableEntry rightPIDError;
-  private NetworkTableEntry leftVelocity;
-  private NetworkTableEntry rightVelocity;
+  private NetworkTableEntry networkTableDistanceKP, networkTableDistanceKI, networkTableDistanceKD, networkTableDistanceKF, leftDistancePIDError, rightDistancePIDError, leftVelocity,rightVelocity;
+  private NetworkTableEntry networkTableTurnKP, networkTableTurnKI, networkTableTurnKD, networkTableTurnKF, leftTurnPIDError, rightTurnPIDError, turnSpeed;
   /**
    * if pid tuning is being done
    */
@@ -81,6 +60,7 @@ public class VelocityDrive extends Subsystem {
    * ticks per 100ms;
    */
   private final double MAX_SPEED = 3628.0;
+  private final double MAX_TURN = 1000; //TODO
   /**
    * Timeout constant for PIDF control in milliseconds
    */
@@ -88,7 +68,10 @@ public class VelocityDrive extends Subsystem {
   /**
    * PIDF constants for velocity control
    */
-  private final double KF = 1023/MAX_SPEED, KP = 0.0, KI = 0.0, KD = 0.0;
+  private final double DISTANCE_KF = 1023/MAX_SPEED, DISTANCE_KP = 0.0, DISTANCE_KI = 0.0, DISTANCE_KD = 0.0, DISTANCE_MAX_OUTPUT = 1.0;
+  private final int PRIMARY_PID_INX = 0;
+  private final double TURN_KF = 0.0, TURN_KP = 0.0, TURN_KI = 0.0, TURN_KD = 0.0, TURN_MAX_OUTPUT = 1.0;
+  private final int TURN_PID_INX = 1;
   /**
    * Deadband constant used to enable quickturning
    */
@@ -125,39 +108,38 @@ public class VelocityDrive extends Subsystem {
     lFront.set(ControlMode.Follower, lFront.getDeviceID());
     rFront.set(ControlMode.Follower, rFront.getDeviceID());
 
-    lBack.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
-    rBack.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+    lBack.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, PRIMARY_PID_INX, kTimeoutMs);
+    rBack.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, PRIMARY_PID_INX, kTimeoutMs);
 
     setBrakeMode(true);
 
-    lBack.config_kF(0, KF, kTimeoutMs);
-    lBack.config_kP(0, KP, kTimeoutMs);
-    lBack.config_kI(0, KI, kTimeoutMs);
-    lBack.config_kD(0, KD, kTimeoutMs);
-
-    rBack.config_kF(0, KF, kTimeoutMs);
-    rBack.config_kP(0, KP, kTimeoutMs);
-    rBack.config_kI(0, KI, kTimeoutMs);
-    rBack.config_kD(0, KD, kTimeoutMs);
+    setDistancePIDFConstants(DISTANCE_KP, DISTANCE_KI, DISTANCE_KD, DISTANCE_KF);
+    setTurnPIDFConstants(TURN_KP, TURN_KI, TURN_KD, TURN_KF);
+    lBack.configAuxPIDPolarity(true, kTimeoutMs);
+    rBack.configAuxPIDPolarity(false, kTimeoutMs);
+    lBack.configClosedLoopPeakOutput(PRIMARY_PID_INX, DISTANCE_MAX_OUTPUT);
+    rBack.configClosedLoopPeakOutput(PRIMARY_PID_INX, DISTANCE_MAX_OUTPUT);
+    lBack.configClosedLoopPeakOutput(TURN_PID_INX, TURN_MAX_OUTPUT);
+    rBack.configClosedLoopPeakOutput(TURN_PID_INX, TURN_MAX_OUTPUT);
     this.PIDFTuning = PIDFTuning;
     if(PIDFTuning) {
       manualPIDFTuner = Shuffleboard.getTab("PIDF Tuner");
-      networkTableKP = manualPIDFTuner.add("KP", KP).getEntry();
-      networkTableKI = manualPIDFTuner.add("KI", KI).getEntry();
-      networkTableKD = manualPIDFTuner.add("KD", KD).getEntry();
-      networkTableKF = manualPIDFTuner.add("KF", KF).getEntry();
-      leftPIDError = manualPIDFTuner.add("Left PID Error", 0.0).getEntry();
-      rightPIDError = manualPIDFTuner.add("Right PID Error", 0.0).getEntry();
+      networkTableDistanceKP = manualPIDFTuner.add("Distance KP", DISTANCE_KP).getEntry();
+      networkTableDistanceKI = manualPIDFTuner.add("Distance KI", DISTANCE_KI).getEntry();
+      networkTableDistanceKD = manualPIDFTuner.add("Distance KD", DISTANCE_KD).getEntry();
+      networkTableDistanceKF = manualPIDFTuner.add("Distance KF", DISTANCE_KF).getEntry();
+      leftDistancePIDError = manualPIDFTuner.add("Left Distance PID Error", 0.0).getEntry();
+      rightDistancePIDError = manualPIDFTuner.add("Right Distance PID Error", 0.0).getEntry();
       leftVelocity = manualPIDFTuner.add("Left Velocity", 0.0).getEntry();
       rightVelocity = manualPIDFTuner.add("Right Velocity", 0.0).getEntry();
     } else {
       manualPIDFTuner = null;
-      networkTableKP = null;
-      networkTableKI = null;
-      networkTableKD = null;
-      networkTableKF = null;
-      leftPIDError = null;
-      rightPIDError = null;
+      networkTableDistanceKP = null;
+      networkTableDistanceKI = null;
+      networkTableDistanceKD = null;
+      networkTableDistanceKF = null;
+      leftDistancePIDError = null;
+      rightDistancePIDError = null;
       leftVelocity = null;
       rightVelocity = null;
     }
@@ -180,8 +162,8 @@ public class VelocityDrive extends Subsystem {
   public void drive(DriveSignal signal) {
     if(PIDFTuning) {
       refreshPIDValuesNetworkTable();
-      leftPIDError.setDouble((double)lBack.getClosedLoopError());
-      rightPIDError.setDouble((double)rBack.getClosedLoopError());
+      leftDistancePIDError.setDouble((double)lBack.getClosedLoopError());
+      rightDistancePIDError.setDouble((double)rBack.getClosedLoopError());
       leftVelocity.setDouble((double)lBack.getSelectedSensorVelocity());
       rightVelocity.setDouble((double)rBack.getSelectedSensorVelocity());
     }
@@ -221,14 +203,14 @@ public class VelocityDrive extends Subsystem {
    * refreshes PID parameters from shuffleboard
    */
   public void refreshPIDValuesNetworkTable() {
-    double KP = this.KP, KI = this.KI, KD = this.KD, KF = this.KF;
+    double KP = DISTANCE_KP, KI = DISTANCE_KI, KD = DISTANCE_KD, KF = DISTANCE_KF;
     if(PIDFTuning) {
-      KP = networkTableKP.getDouble(KP);
-      KI = networkTableKI.getDouble(KI);
-      KD = networkTableKD.getDouble(KD);
-      KF = networkTableKF.getDouble(KF);
+      KP = networkTableDistanceKP.getDouble(KP);
+      KI = networkTableDistanceKI.getDouble(KI);
+      KD = networkTableDistanceKD.getDouble(KD);
+      KF = networkTableDistanceKF.getDouble(KF);
       
-      setPIDFConstants(KP, KI, KD, KF);
+      setDistancePIDFConstants(KP, KI, KD, KF);
     }
   }
 
@@ -239,16 +221,28 @@ public class VelocityDrive extends Subsystem {
    * @param KD D parameter
    * @param DF F parameter
    */
-  public void setPIDFConstants(double KP, double KI, double KD, double KF) {
-    lBack.config_kF(0, KF, kTimeoutMs);
-    lBack.config_kP(0, KP, kTimeoutMs);
-    lBack.config_kI(0, KI, kTimeoutMs);
-    lBack.config_kD(0, KD, kTimeoutMs);
+  public void setDistancePIDFConstants(double KP, double KI, double KD, double KF) {
+    lBack.config_kF(PRIMARY_PID_INX, KF, kTimeoutMs);
+    lBack.config_kP(PRIMARY_PID_INX, KP, kTimeoutMs);
+    lBack.config_kI(PRIMARY_PID_INX, KI, kTimeoutMs);
+    lBack.config_kD(PRIMARY_PID_INX, KD, kTimeoutMs);
 
-    rBack.config_kF(0, KF, kTimeoutMs);
-    rBack.config_kP(0, KP, kTimeoutMs);
-    rBack.config_kI(0, KI, kTimeoutMs);
-    rBack.config_kD(0, KD, kTimeoutMs);
+    rBack.config_kF(PRIMARY_PID_INX, KF, kTimeoutMs);
+    rBack.config_kP(PRIMARY_PID_INX, KP, kTimeoutMs);
+    rBack.config_kI(PRIMARY_PID_INX, KI, kTimeoutMs);
+    rBack.config_kD(PRIMARY_PID_INX, KD, kTimeoutMs);
+  }
+
+  public void setTurnPIDFConstants(double KP, double KI, double KD, double KF) {
+    lBack.config_kF(TURN_PID_INX, KF, kTimeoutMs);
+    lBack.config_kP(TURN_PID_INX, KP, kTimeoutMs);
+    lBack.config_kI(TURN_PID_INX, KI, kTimeoutMs);
+    lBack.config_kD(TURN_PID_INX, KD, kTimeoutMs);
+
+    rBack.config_kF(TURN_PID_INX, KF, kTimeoutMs);
+    rBack.config_kP(TURN_PID_INX, KP, kTimeoutMs);
+    rBack.config_kI(TURN_PID_INX, KI, kTimeoutMs);
+    rBack.config_kD(TURN_PID_INX, KD, kTimeoutMs);
   }
 
   @Override
